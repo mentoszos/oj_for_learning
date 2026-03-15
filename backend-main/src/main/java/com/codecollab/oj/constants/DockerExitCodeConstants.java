8bf33d4d5e64b1312ac6a35b715aa0beb569efc7
mentos <89824009+mentoszos@users.noreply.github.com>
2026-03-15 19:09:24 +0800

    前端进来，保留的是httpdebug的版本；测试压力跟tomcat线程数无关，机子性能问题。

---
+++ backend-main/src/main/java/com/codecollab/oj/constants/DockerExitCodeConstants.java
@@ -0,0 +1,12 @@
+package com.codecollab.oj.constants;
+
+public final class DockerExitCodeConstants {
+
+    private DockerExitCodeConstants() {
+    }
+
+    public static final int SUCCESS = 0;
+    public static final int CE_OR_RE = 1;
+    public static final int MLE = 137;
+    public static final int UNKNOWN = -1;
+}
